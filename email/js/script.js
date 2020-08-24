3df147f4d694856e6572a3811024365be9df3746
davide-amabile <davideamabile81@gmail.com>
2020-08-24 15:56:32 +0200

    esrecizio email

--- email/js/script.js
+++ email/js/script.js
@@ -4,17 +4,17 @@ var email = prompt("Inserire la tua email");
 // creare array email
 var listEmail = ["azzurro@gmail.com", "rosso@gmail.com", "giallo@gmail.com"];
 
-mailSistema = false;
+var mailSistema = false;
 
 // controllare se l'email è nella listEmail
 for (var i = 0; i < listEmail.length; i++) {
   if (email === listEmail[i]) {
-    mailSistema = true;
+   var  mailSistema = true;
   }
 }
 
-if (mailSistema = true) {
+if (  mailSistema = true) {
   console.log("benvenuto");
-} else if (mailSistema != true) {
+} else if ( mailSistema = false) {
   console.log("reinserisci l'email");
 }
