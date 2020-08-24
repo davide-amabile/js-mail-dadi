ec2ba2572bf992412b44d348eb2ba3205dbd8a6f
davide-amabile <davideamabile81@gmail.com>
2020-08-24 15:49:02 +0200

    esercizio email

--- email/js/script.js
+++ email/js/script.js
@@ -4,12 +4,17 @@ var email = prompt("Inserire la tua email");
 // creare array email
 var listEmail = ["azzurro@gmail.com", "rosso@gmail.com", "giallo@gmail.com"];
 
+mailSistema = false;
+
 // controllare se l'email è nella listEmail
 for (var i = 0; i < listEmail.length; i++) {
   if (email === listEmail[i]) {
-    console.log("Benvenuto");
+    mailSistema = true;
   }
-  else if (email != listEmail[i]) {
-    console.log("Riprovare a reinserire l'email");
-  }
+}
+
+if (mailSistema = true) {
+  console.log("benvenuto");
+} else if (mailSistema != true) {
+  console.log("reinserisci l'email");
 }
